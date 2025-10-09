67f307154b2e23edf6c89a0674021eb7cf26c7c8
Vipin Yadav <TVDMUMVY838@techvedgroup.com>
2025-10-09 17:05:54 +0530

    Update todo app: modal, list, tasks functionality

--- 03-todo/src/components/Modal.jsx
+++ 03-todo/src/components/Modal.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Modal = ({closeMod}) => {
+const Modal = ({ closeMod, addTask }) => {
+  const [taskInput, settaskInput] = useState("");
+
+  const handleTask = () => {
+    if (taskInput.trim() != "") {
+      addTask(taskInput);
+      settaskInput("");
+      closeMod()
+    }
+  };
+
   return (
-    <div className='modal-div'>
-        <h2>New Note</h2>
-        <input type="text" />
-        <div className='modal-btns'>
-            <button onClick={closeMod}  className='modal-cancel-btn'>Cancel</button>
-            <button className='modal-add-btn'>Add</button>
-        </div>
+    <div className="modal-div">
+      <h2>New Note</h2>
+      <input
+        type="text"
+        value={taskInput}
+        onChange={(e) => settaskInput(e.target.value)}
+      />
+      <div className="modal-btns">
+        <button onClick={closeMod} className="modal-cancel-btn">
+          Cancel
+        </button>
+        <button onClick={handleTask} className="modal-add-btn">
+          Add
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
