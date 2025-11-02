64a908580e085242363ccc661aefd1fa6303e4fb
Vipin Yadav <vipinvyadav9@gmail.com>
2025-11-02 21:00:50 +0530

    first Deployment

--- evolution/components/common/Header/Header.jsx
+++ evolution/components/common/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Header.module.css'
 import Container from '../Container/Container'
 import CommonPopup from '../CommonPopup/CommonPopup'
@@ -9,13 +9,35 @@ const Header = () => {
   //   setisPopup(!isPopup);
   // }
 
+  const [scroll , setScroll] = useState(false);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY && currentScrollY > 0) {
+          // Scrolling down
+          setScroll(true);
+        } else {
+          // Scrolling up or at top
+          setScroll(false);
+        }
+
+        setLastScrollY(currentScrollY);
+      };
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }, [lastScrollY]);
+
   const [openMenu , setOpenMenu] = useState(false);
   const toggleMenu = () =>{
     setOpenMenu(!openMenu);
   }
   return (
     <>
-    <div className={styles.header}>
+    <div className={`${styles.header} ${scroll ? styles.headerNone : ""}`}>
       <Container>
         <img src="/images/header/logo.svg" alt="Logo" />
         <img onClick={toggleMenu} src="/images/header/menu.svg" alt="Menu" />
